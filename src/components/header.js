c3e7e8d24c21dcb1e99afc481252a361f31d51a9
Lucian Carp <lc16929@my.bristol.ac.uk>
2019-08-08 09:13:15 +0300

    Use styled in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-scroll'
 import Scrollspy from 'react-scrollspy'
 
-import headerStyles from './header.module.scss'
+import { colors, screenSizes } from '../styles/global'
 
 const Header = () => (
-  <header className={headerStyles.container}>
-    <nav className={headerStyles.navMenu}>
-      <Scrollspy
+  <Container>
+    <NavMenu>
+      <NavList
         items={['main', 'work', 'contact']}
-        currentClassName={headerStyles.isCurrent}
-        className={headerStyles.navList}
+        currentClassName="is-current"
         offset={-20}
       >
-        <li className={headerStyles.navItem}>
+        <NavItem>
           <Link
             activeClass="active"
             to="main"
@@ -24,8 +24,8 @@ const Header = () => (
           >
             <h2>Main</h2>
           </Link>
-        </li>
-        <li className={headerStyles.navItem}>
+        </NavItem>
+        <NavItem>
           <Link
             activeClass="active"
             to="work"
@@ -36,8 +36,8 @@ const Header = () => (
           >
             <h2>Work</h2>
           </Link>
-        </li>
-        <li className={headerStyles.navItem}>
+        </NavItem>
+        <NavItem>
           <Link
             activeClass="active"
             to="contact"
@@ -48,10 +48,81 @@ const Header = () => (
           >
             <h2>Contact</h2>
           </Link>
-        </li>
-      </Scrollspy>
-    </nav>
-  </header>
+        </NavItem>
+      </NavList>
+    </NavMenu>
+  </Container>
 )
+
+const Container = styled.header`
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  margin-left: -1rem;
+  margin-right: -1rem;
+
+  background-color: ${colors.background};
+  min-height: 10vh;
+  display: flex;
+  align-items: center;
+
+  @supports (backdrop-filter: none) {
+    background-color: ${colors.backgroundTransp};
+    backdrop-filter: blur(5px);
+  }
+`
+
+const NavMenu = styled.nav`
+  margin-top: auto;
+  margin-bottom: auto;
+
+  .is-current {
+    color: ${colors.primary};
+  }
+`
+
+const NavList = styled(Scrollspy)`
+  display: flex;
+  list-style-type: none;
+  margin: 0;
+`
+
+const NavItem = styled.li`
+  margin-top: auto;
+  margin-bottom: auto;
+  cursor: pointer;
+
+  transition: color 0.2s;
+  -webkit-transition: color 0.2s;
+  transition-timing-function: ease-out;
+
+  @media (min-width: ${screenSizes.laptop}) {
+    &:hover {
+      color: ${colors.primary};
+    }
+  }
+
+  h2 {
+    margin: 0;
+    margin-right: 1rem;
+    display: inline-block;
+
+    font-size: 1.2rem;
+    border-bottom: 0.15rem solid ${colors.text};
+    line-height: 2rem;
+
+    @media (min-width: ${screenSizes.mobileL}) {
+      font-size: 1.62671rem;
+      line-height: 2.5rem;
+    }
+  }
+`
 
 export default Header
