562cd5eb5a2cadf4beed8d7a7599adf2c1a2f318
Lucian Carp <lc16929@my.bristol.ac.uk>
2019-08-08 08:49:33 +0300

    Use createGlobalStyle in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled, { createGlobalStyle } from 'styled-components'
 
 import '../styles/reset.scss'
-import layoutStyles from './layout.module.scss'
+import { fontSizes, colors, fonts } from '../styles/global'
 import Footer from './footer'
 
 // import { useStaticQuery, graphql } from 'gatsby'
@@ -15,19 +16,72 @@ const Layout = ({ children }) => {
   return (
     <>
       {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
-      <motion.div
-        className={layoutStyles.container}
+      <GlobalStyle />
+      <Container
         initial="hidden"
         animate="visible"
         variants={opacityVariants}
         transition={{ duration: 0.4, ease: 'easeOut' }}
       >
-        <main className={layoutStyles.content}>{children}</main>
+        <Content>{children}</Content>
         <Footer />
-      </motion.div>
+      </Container>
     </>
   )
 }
+
+const Container = styled(motion.div)`
+  margin: 0 auto;
+  max-width: 1024px;
+  padding: 1rem;
+  padding-top: 0;
+  padding-bottom: 0;
+
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+
+const Content = styled.main`
+  flex-grow: 1;
+  max-width: 600px;
+`
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Arvo:400i&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:900&display=swap');
+
+  body,
+  html,
+  #root {
+    font-family: ${fonts.body};
+    color: ${colors.text};
+    background-color: ${colors.background};
+    font-size: ${fontSizes.regular};
+    font-weight: 400;
+    font-style: italic;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: ${fonts.header};
+    text-transform: uppercase;
+    font-style: normal;
+    letter-spacing: 0.1rem;
+  }
+
+  h1 {
+    letter-spacing: 0.2rem;
+  }
+
+  p {
+    text-align: justify;
+  }
+`
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { createGlobalStyle } from 'styled-components'
-import { fontSizes, colors, fonts } from '../styles/global'
 
 import Layout from '../components/layout'
 // import Image from '../components/image'
@@ -12,53 +10,14 @@ import LandingMain from '../components/landing-main'
 import About from '../components/about'
 
 const IndexPage = () => (
-  <React.Fragment>
-    <GlobalStyle />
-    <Layout>
-      <SEO title="Home" />
-      <LandingMain />
-      <Header />
-      <About />
-      <Work />
-      <Contact />
-    </Layout>
-  </React.Fragment>
+  <Layout>
+    <SEO title="Home" />
+    <LandingMain />
+    <Header />
+    <About />
+    <Work />
+    <Contact />
+  </Layout>
 )
-
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Arvo:400i&display=swap');
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:900&display=swap');
-
-  body,
-  html,
-  #root {
-    font-family: ${fonts.body};
-    color: ${colors.text};
-    background-color: ${colors.background};
-    font-size: ${fontSizes.regular};
-    font-weight: 400;
-    font-style: italic;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    font-family: ${fonts.header};
-    text-transform: uppercase;
-    font-style: normal;
-    letter-spacing: 0.1rem;
-  }
-
-  h1 {
-    letter-spacing: 0.2rem;
-  }
-
-  p {
-    text-align: justify;
-  }
-`
 
 export default IndexPage
