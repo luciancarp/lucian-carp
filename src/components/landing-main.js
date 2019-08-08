09a3bf6083211036982d07081d2081016dcd00fe
Lucian Carp <lc16929@my.bristol.ac.uk>
2019-08-08 08:58:36 +0300

    Use styled in landing-main

--- src/components/landing-main.js
+++ src/components/landing-main.js
@@ -1,15 +1,39 @@
 import React from 'react'
-import landingMainStyles from './landing-main.module.scss'
+import styled from 'styled-components'
+import { colors, fontSizes, screenSizes } from '../styles/global'
 
 const LandingMain = () => {
   return (
-    <section className={landingMainStyles.container} id={'main'}>
-      <div className={landingMainStyles.header}>
-        <h1>Lucian Carp</h1>
-        <h2>Full-stack developer</h2>
-      </div>
-    </section>
+    <Container id={'main'}>
+      <MainTitle>Lucian Carp</MainTitle>
+      <h2>Full-stack developer</h2>
+    </Container>
   )
 }
+
+const Container = styled.section`
+  padding-top: 10vh;
+  min-height: 30vh;
+`
+
+const MainTitle = styled.h1`
+  display: inline-block;
+  color: ${colors.primary};
+  border-bottom: 0.15rem solid ${colors.text};
+
+  font-size: 2rem;
+  line-height: 3rem;
+  margin-bottom: 1rem;
+
+  @media (min-width: ${screenSizes.mobileL}) {
+    font-size: ${fontSizes.xLarge};
+    line-height: 4rem;
+  }
+
+  @media (min-width: ${screenSizes.laptop}) {
+    font-size: 4rem;
+    line-height: 5rem;
+  }
+`
 
 export default LandingMain
