decdad11aecd0b178893a085aad4b6c1e2870c51
Lucian Carp <lc16929@my.bristol.ac.uk>
2019-08-11 10:39:47 +0300

    Add back button in project template

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 import { graphql } from 'gatsby'
+import { screenSizes } from '../styles/global'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { Gallery } from '../components/gallery'
+import Left from '../assets/left.svg'
 
 // GatsbyContentfulFluid not working in graphiql or playground
 export const query = graphql`
@@ -25,7 +27,14 @@ const Project = props => {
     <Layout>
       <SEO title={props.data.contentfulProject.title} />
       <ProjectContainer>
-        <h1>{props.data.contentfulProject.title}</h1>
+        <Title>
+          {/* <Link to={`/`}> */}
+          <LeftButton onClick={() => window.history.back()}>
+            <StyledLeft />
+          </LeftButton>
+          {/* </Link> */}
+          <h1>{props.data.contentfulProject.title}</h1>
+        </Title>
         <Gallery images={props.data.contentfulProject.images} />
         <p>{props.data.contentfulProject.description}</p>
         <p>{props.data.contentfulProject.description}</p>
@@ -37,6 +46,46 @@ const Project = props => {
     </Layout>
   )
 }
+
+const Title = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+
+  h1 {
+    margin: 0;
+  }
+
+  margin-bottom: 1.5rem;
+`
+
+const StyledLeft = styled(Left)`
+  height: 1.5rem;
+  width: 1.5rem;
+  margin-right: 0.5rem;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+`
+
+const LeftButton = styled.div`
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  fill: ${props => props.theme.text};
+  transition: fill 0.2s;
+  -webkit-transition: fill 0.2s;
+  transition-timing-function: ease-out;
+
+  @media (min-width: ${screenSizes.laptop}) {
+    &:hover {
+      fill: ${props => props.theme.primary};
+    }
+  }
+`
 
 const ProjectContainer = styled.div`
   padding-top: 10vh;
