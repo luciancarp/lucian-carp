96726196ef14891551865ed1edc8454a0d2cb9b4
Lucian Carp <lc16929@my.bristol.ac.uk>
2019-08-11 15:35:29 +0300

    Move down header in 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,18 @@
 import React from 'react'
-
+import styled from 'styled-components'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
+    <Title>NOT FOUND</Title>
     <p>You just hit a route that doesn&#39;t exist...</p>
   </Layout>
 )
+
+const Title = styled.h1`
+  margin-top: 10vh;
+`
 
 export default NotFoundPage
