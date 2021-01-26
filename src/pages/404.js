8b2ab5aa4f3f3183101924dd20c8da2b37f2a2eb
Lucian Carp <lucian.g.carp@gmail.com>
2021-01-26 19:21:37 +0200

    Stop using layout in 404 and thanks

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
-import Layout from '../components/layout'
 import SEO from '../components/seo'
 import StyledLink from '../components/styled-link'
 
 const NotFoundPage = () => (
-  <Layout>
+  <Container>
     <SEO title="404: Not found" />
     <Title>NOT FOUND</Title>
     <p>You just hit a route that doesn&#39;t exist...</p>
     <StyledLink destination="/" title="home" />
-  </Layout>
+  </Container>
 )
 
 const Title = styled.h1`
   margin-top: 10vh;
+`
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `
 
 export default NotFoundPage
--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
-import Layout from '../components/layout'
 import SEO from '../components/seo'
 import StyledLink from '../components/styled-link'
 
 const Thanks = () => (
-  <Layout>
+  <Container>
     <SEO title="Thank you!" />
     <Title>Thank you!</Title>
     <p>Thank you for contacting me. I will get in touch with you shortly.</p>
     <StyledLink destination="/" title="home" />
-  </Layout>
+  </Container>
 )
 
 const Title = styled.h1`
   margin-top: 10vh;
+`
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `
 
 export default Thanks
